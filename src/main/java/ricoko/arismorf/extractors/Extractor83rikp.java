693d19f85a309a9d6677c59c68c18c4b02207413
IvanSlastikhin <stopgo89@gmail.com>
2011-09-11 14:15:21 +0400

    Фикс

--- src/main/java/ricoko/arismorf/extractors/Extractor83rikp.java
+++ src/main/java/ricoko/arismorf/extractors/Extractor83rikp.java
@@ -4,10 +4,54 @@
  */
 package ricoko.arismorf.extractors;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JTextArea;
+import ricoko.arismorf.ExcelExport;
+
 /**
  *
  * @author accepted
  */
 public class Extractor83rikp {
+    
+    private static ResultSet[] rs;
+    private static Statement[] s;
+    private static ExcelExport ee;
+    private static int STCOUNT = 5;
+    private static JTextArea log;
+    
+     public static void extract(Connection c, JTextArea log) throws SQLException, FileNotFoundException, IOException {
+        ee = new ExcelExport();
+        Extractor83rikp.log = log;
+        if (log != null) {
+            log.append("Заполняю форму ОШ 5.\n");
+        }
+        rs = new ResultSet[STCOUNT];
+        s = new Statement[STCOUNT];
+        for (int i = 0; i < STCOUNT; i++) {
+            s[i] = c.createStatement();
+        }
+
+        ee.saveExportedWorkBooks(log);
+
+        if (log != null) {
+            log.append("Экпорт завершен. Результаты экспорта располагаются в папке export.\n");
+        }
+        for (int i = 0; i < STCOUNT; i++) {
+            s[i].close();
+        }
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().open(new File("export"));
+        }
+        
+        
+    }
     
 }
