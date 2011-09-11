ca14e6bfc5dbe15ca2c7feb01bc0ef63c75b8a50
IvanSlastikhin <stopgo89@gmail.com>
2011-09-11 14:12:08 +0400

    83РИК

---
+++ src/main/java/ricoko/arismorf/extractors/Extractor83rikp.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ricoko.arismorf.extractors;
+
+/**
+ *
+ * @author accepted
+ */
+public class Extractor83rikp {
+    
+}
